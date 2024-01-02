d7f17abe54785a350e04a884c9e6a42dedad61a0
MabdullahK <105320392+MabullahK@users.noreply.github.com>
2024-01-02 15:44:10 +0000

    Create Tkinter_list.py
    A list of Titanfall 2 titans using Tkinter

---
+++ Tkinter_list.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+home = Tk()
+
+List = Listbox(home)
+
+List.insert(1,"Ion")
+List.insert(2,"Tone")
+List.insert(3,"Monarch")
+List.insert(4,"Ronin")
+List.insert(5,"Northstar")
+List.insert(6,"Scorch")
+List.insert(7,"Leigon")
+
+List.pack()
+
+home.mainloop()
+
